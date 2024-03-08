9e95eda66bae31caf329ce843dc10c6ae2d4be64
petr <petr.cech@netbox.cz>
2024-03-08 13:38:55 +0100

    added django-extensions

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movies'
+
+    'django_extensions',
+
+    'movies',
 ]
 
 MIDDLEWARE = [
